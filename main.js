23c647f02c585f9470b2c6975b87bbde4416d93e
Wei Lin <weisl9723@gmail.com>
2022-06-15 21:23:07 -0500

    added basic calculator style to page only

---
+++ main.js
@@ -0,0 +1,15 @@
+function addition(valueOne, valueTwo) {
+    return valueOne + valueTwo;
+}
+
+function subtract(valueOne, valueTwo) {
+    return valueOne - valueTwo;
+}
+
+function multiply(valueOne, valueTwo) {
+    return valueOne * valueTwo;
+}
+
+function divide(valueOne, valueTwo) {
+    return valueOne / valueTwo;
+}
